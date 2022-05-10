472932a0dcfd8055456852c80e78c2262dbc69da
Tophuu <95060057+Tophuu@users.noreply.github.com>
2022-05-09 19:38:54 -0700

    Added Junit Testing stuff

--- Source Code/Driver.java
+++ Source Code/Driver.java
@@ -1,4 +1,3 @@
-
 public class Driver 
 {
     static Graph<String> createGraph(){
---
+++ Source Code/DriverTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DriverTest 
+{
+
+    @Test
+    public void testbreadthFirstTraversal()
+    {
+
+    }
+
+    @Test
+    public void testdepthFirstTraversal()
+    {
+
+    }
+}
